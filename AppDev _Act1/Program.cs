0b93d26be0773e8fd1def90da030555e37a87071
Redd <reddreyes13@gmail.com>
2025-09-20 21:59:29 +0800

    Add project files.

---
+++ AppDev _Act1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppDev_Act1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+
+            Console.Write("Enter first number: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            string operation;
+
+            Console.Write("Enter operation (+, -, *, /): ");
+            operation = Console.ReadLine();
+
+            if (operation == "+")
+            {
+                Console.WriteLine($"Result: {x + y}");
+            }
+            else if (operation == "-")
+            {
+                Console.WriteLine($"Result: {x - y}");
+            }
+            else if (operation == "*")
+            {
+                Console.WriteLine($"Result: {x * y}");
+            }
+            else if (operation == "/")
+            {
+                if (y != 0)
+                {
+                    Console.WriteLine($"Result: {x / y}");
+                }
+                else
+                {
+                    Console.WriteLine("Error: Division by zero is not allowed.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error: Invalid operation.");
+            }   
+        }
+    }
+}
